f55c5c2f42327ecb6742e386ea709b5b1875a447
Uveso <Archivar@Zwergenteam.de>
2022-08-22 05:10:41 +0200

    UPDATE v19
    - Unit eal0001 (Aeon Armored Command Unit) Changed sizeX to 0.18
    - fixed missing unit.FactionCategory

--- hook/lua/defaultunits.lua
+++ hook/lua/defaultunits.lua
@@ -3,12 +3,11 @@ local oldACUUnit = ACUUnit
 ACUUnit = Class(oldACUUnit) {    
     updateBuildRestrictions = function(self)
         local aiBrain = self:GetAIBrain()
-
+        local factionCategory = categories[string.upper(__blueprints[self.UnitId].General.FactionName)]
         -- Sanity check.
-        if not self.FactionCategory then
+        if not factionCategory then
             return
         end
-
         self:AddBuildRestriction(categories.SUPPORTFACTORY)
         
         local upgradeNames = {
@@ -23,26 +22,26 @@ ACUUnit = Class(oldACUUnit) {
         -- Check for the existence of HQs
         for i, researchType in ipairs({categories.LAND, categories.AIR, categories.NAVAL}) do
             -- If there is a research station of the appropriate type, enable support factory construction
-            for id, unit in aiBrain:GetListOfUnits(categories.RESEARCH * categories.TECH2 * self.FactionCategory * researchType, false, true) do
+            for id, unit in aiBrain:GetListOfUnits(categories.RESEARCH * categories.TECH2 * factionCategory * researchType, false, true) do
                 if not unit.Dead and not unit:IsBeingBuilt() then
                     for key, title in upgradeNames do
                         if self:HasEnhancement(title) then
-                            self:RemoveBuildRestriction(categories.TECH2 * categories.SUPPORTFACTORY * self.FactionCategory * researchType)
+                            self:RemoveBuildRestriction(categories.TECH2 * categories.SUPPORTFACTORY * factionCategory * researchType)
                             break
                         end
                     end
                     break
                 end
             end
-            for id, unit in aiBrain:GetListOfUnits(categories.RESEARCH * categories.TECH3 * self.FactionCategory * researchType, false, true) do
+            for id, unit in aiBrain:GetListOfUnits(categories.RESEARCH * categories.TECH3 * factionCategory * researchType, false, true) do
                 if not unit.Dead and not unit:IsBeingBuilt() then
                     -- Special case for the commander, since its engineering upgrades are implemented using build restrictions
                     for key, title in upgradeNames do
                         if key <= 2 and self:HasEnhancement(title) then
-                            self:RemoveBuildRestriction(categories.TECH2 * categories.SUPPORTFACTORY * self.FactionCategory * researchType)
+                            self:RemoveBuildRestriction(categories.TECH2 * categories.SUPPORTFACTORY * factionCategory * researchType)
                         elseif key > 2 and self:HasEnhancement(title) then
-                            self:RemoveBuildRestriction(categories.TECH2 * categories.SUPPORTFACTORY * self.FactionCategory * researchType)
-                            self:RemoveBuildRestriction(categories.TECH3 * categories.SUPPORTFACTORY * self.FactionCategory * researchType)
+                            self:RemoveBuildRestriction(categories.TECH2 * categories.SUPPORTFACTORY * factionCategory * researchType)
+                            self:RemoveBuildRestriction(categories.TECH3 * categories.SUPPORTFACTORY * factionCategory * researchType)
                             break
                         end
                     end
